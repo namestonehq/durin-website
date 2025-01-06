1f33a6b42bc1758c68db245a9209c7823cf4f6d5
Darian Bailey <darian@nftychat.xyz>
2025-01-06 11:57:18 -0500

    enable all chains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,14 +188,6 @@ export default function Home() {
                   onClick={() => {
                     // Set chainModifier to empty string meaining mainnet
                     setChainModifier("");
-                    // if chainName is optimisim, arbitrum, or linea, set it to base
-                    if (
-                      chainName === "Optimism" ||
-                      chainName === "Arbitrum" ||
-                      chainName === "Linea"
-                    ) {
-                      setChainName("Base");
-                    }
                   }}
                   className={`px-4  rounded transition ${
                     chainModifier === ""
@@ -247,15 +239,10 @@ export default function Home() {
               </button>
               <button
                 onClick={() => setChainName("Optimism")}
-                disabled={chainModifier !== "Sepolia"}
                 className={`px-4 rounded transition ${
                   chainName === "Optimism"
                     ? "bg-white shadow text-stone-900"
                     : "opacity-50"
-                } ${
-                  chainModifier !== "Sepolia"
-                    ? "cursor-not-allowed filter blur-[2px]"
-                    : ""
                 }`}
               >
                 <Image
@@ -268,16 +255,11 @@ export default function Home() {
               </button>
               <button
                 onClick={() => setChainName("Arbitrum")}
-                disabled={chainModifier !== "Sepolia"}
                 className={`px-4 rounded transition ${
                   chainName === "Arbitrum"
                     ? "bg-white shadow text-stone-900"
                     : "opacity-50"
-                } ${
-                  chainModifier !== "Sepolia"
-                    ? "cursor-not-allowed filter blur-[2px]"
-                    : ""
-                }`}
+                } `}
               >
                 <Image
                   src="/arbitrum.svg"
@@ -290,16 +272,11 @@ export default function Home() {
 
               <button
                 onClick={() => setChainName("Linea")}
-                disabled={chainModifier !== "Sepolia"}
                 className={`px-4 rounded transition ${
                   chainName === "Linea"
                     ? "bg-white shadow text-stone-900"
                     : "opacity-50"
-                } ${
-                  chainModifier !== "Sepolia"
-                    ? "cursor-not-allowed filter blur-[2px]"
-                    : ""
-                }`}
+                } `}
               >
                 <Image
                   src="/linea.svg"
