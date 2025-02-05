ba5530a103990926aecf8872af13620cd790ba92
aslobodnik <aslobodnik@gmail.com>
2025-02-05 17:00:41 -0500

    adjust deploy button width

--- src/app/components/deploy-button.tsx
+++ src/app/components/deploy-button.tsx
@@ -200,7 +200,7 @@ const DeployButton: React.FC<DeployButtonProps> = ({
     <button
       onClick={handleDeploy}
       disabled={isDisabled}
-      className={`py-1 text-sm w-20 border rounded text-stone-900 ${
+      className={`py-1 text-sm w-24 border rounded text-stone-900 ${
         isDisabled ? "bg-stone-100 cursor-not-allowed" : "hover:bg-stone-100"
       }`}
     >
